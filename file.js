295d7fdf6aec553385dad64825f9da5c07cf49c8
hacene <larabi1603@gmail.com>
2022-10-27 14:48:08 -0400

    storing objects on js

--- file.js
+++ file.js
@@ -7,8 +7,47 @@ const dateA = document.getElementById('datea');
  const message = document.getElementById('message');
 
 
+ 
+
+function myFunct(){
+    var distination = document.getElementById('distination').value;
+    var fdist = document.getElementById('fdist').value;
+    var date = document.getElementById('date').value;
+    var dateA = document.getElementById('datea').value;
+
+    var distinationa = localStorage.setItem("distination", distination);
+    var fdista = localStorage.setItem("fdist", fdist);
+    var datea = localStorage.setItem("date", date);
+    var dateaa = localStorage.setItem("dateA", dateA);
+    
+}
+form.addEventListener('submit', myFunct)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 form.addEventListener('submit', function(e) {
+  
    e.preventDefault();
     if(distination.value === '' || fdist.value === ""){
     alert('the form should be filled out');
@@ -29,9 +68,39 @@ form.addEventListener('submit', function(e) {
         error.push('please select the arrival date');
     } 
     message.innerText = error
-    if(distination.value && fdist.value && date.value && dateA.value){
-    alert('form is submitted')
+    if(distination.value && fdist.value && date.value && datea.value){
+        error.push("thank you for submitting the form")
     }
-   
-    
+message.innerText = error
  });
+
+
+
+
+ /*function show(e){
+    e.preventDefault();
+    submit.innerText = 'please fill out this feild';
+    submit.style.color= 'red';
+    submit.style.backgroundColor = "blue";
+ }
+ submit.addEventListener('click', show);*/
+ 
+
+ /*function showInfo(e){
+    e.preventDefault();
+    var error = []
+    if(distination.value === "" && fdist.value === '' && date.value === "" && dateA.value === ""){
+        error.push( 'please fill out all the feilds');
+   
+    }  else
+    if(distination.value === "" || fdist.value === '' || date.value === "" || datea.value === ""){
+        error.push("fill out the rest of the feilds");
+    }
+    message.innerText = error
+    if(distination.value && fdist.value && date.value && datea.value){
+        alert('your form has been submitted')
+    }
+ }
+ form.addEventListener('submit', showInfo);*/
+
+
